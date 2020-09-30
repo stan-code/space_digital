1f4dec5547aa392365707d3c0cc3e4ce427fc7b8
Stan-Gogan <sjgogan@me.com>
2020-09-30 13:35:44 +1300

    Updates

---
+++ precache-manifest.de33362c25065f4c558585a6ba8da844.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "762613da2ef43efc80a5a4a32e5f44fa",
+    "url": "/space_digital/index.html"
+  },
+  {
+    "revision": "38008040cad47e422a94",
+    "url": "/space_digital/static/css/main.40771c32.chunk.css"
+  },
+  {
+    "revision": "491badb8ba3525991a0b",
+    "url": "/space_digital/static/js/2.fb3ebdc0.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/space_digital/static/js/2.fb3ebdc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38008040cad47e422a94",
+    "url": "/space_digital/static/js/main.1be1db79.chunk.js"
+  },
+  {
+    "revision": "48a3a55d1867e3000f06",
+    "url": "/space_digital/static/js/runtime-main.af7e8cc7.js"
+  },
+  {
+    "revision": "d152081af3751a39f273dc73880e003f",
+    "url": "/space_digital/static/media/BBACMockups.d152081a.png"
+  },
+  {
+    "revision": "24ab5d0715bbed3be5e0eb852ff5c67a",
+    "url": "/space_digital/static/media/Bath Bomb and Chill.24ab5d07.png"
+  },
+  {
+    "revision": "e9e57039a252d39b670eae4b42a00568",
+    "url": "/space_digital/static/media/Final Logo.e9e57039.jpg"
+  },
+  {
+    "revision": "d6605f9d70ade7b3cae066c217d46aec",
+    "url": "/space_digital/static/media/JustinesNpd.d6605f9d.png"
+  },
+  {
+    "revision": "035ac007e7e95adcd3a4114a2609aaa6",
+    "url": "/space_digital/static/media/MoogMockups.035ac007.png"
+  },
+  {
+    "revision": "9ce3aead876095e1cf38dd536bcc482a",
+    "url": "/space_digital/static/media/SabbyMockups.9ce3aead.png"
+  },
+  {
+    "revision": "efd1a3bff67a70d8fd14a74169536175",
+    "url": "/space_digital/static/media/SafeMockups.efd1a3bf.png"
+  },
+  {
+    "revision": "61b73226d35c988a5af6d41fabb5979b",
+    "url": "/space_digital/static/media/TomTomMenu.61b73226.png"
+  },
+  {
+    "revision": "1731314097fff6598bcbaddb8e742525",
+    "url": "/space_digital/static/media/desi.17313140.png"
+  },
+  {
+    "revision": "344035d6a108696f323dd7127893a396",
+    "url": "/space_digital/static/media/desiMockup.344035d6.png"
+  },
+  {
+    "revision": "d58d7446ccd32a87b55cb3997c90f17f",
+    "url": "/space_digital/static/media/dp.d58d7446.jpg"
+  },
+  {
+    "revision": "df9be85b97ff6722115b288ab735c354",
+    "url": "/space_digital/static/media/eurosport.df9be85b.png"
+  },
+  {
+    "revision": "3d2ca07b28bbf84002b8f7852df6cc4f",
+    "url": "/space_digital/static/media/graphicexamples.3d2ca07b.png"
+  },
+  {
+    "revision": "11d671d69a676e08d62f4320005bca7d",
+    "url": "/space_digital/static/media/justines.11d671d6.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/space_digital/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "91e4c9f306f65f8282ec5ac2fd5fd876",
+    "url": "/space_digital/static/media/logo.91e4c9f3.png"
+  },
+  {
+    "revision": "de769f498172c7a1c8ecfc0ff0983da6",
+    "url": "/space_digital/static/media/logodesigns.de769f49.png"
+  },
+  {
+    "revision": "fa60491fbf5ee6eb91f0f7762ba09b97",
+    "url": "/space_digital/static/media/midback.fa60491f.png"
+  },
+  {
+    "revision": "5659f55e6f4bdfeacf0a0c9415b99b9c",
+    "url": "/space_digital/static/media/moog.5659f55e.png"
+  },
+  {
+    "revision": "df50e2e31594f9be26a4a68cc8ea478e",
+    "url": "/space_digital/static/media/safelogo.df50e2e3.png"
+  },
+  {
+    "revision": "d02a5357817746d0b75408313dc38f0e",
+    "url": "/space_digital/static/media/screencap.d02a5357.png"
+  },
+  {
+    "revision": "e4427f9ad3f77d6d7974c64f4d668092",
+    "url": "/space_digital/static/media/timlogo.e4427f9a.png"
+  },
+  {
+    "revision": "c5c0d3e5ca6c205e401fe719d2413880",
+    "url": "/space_digital/static/media/tomtom.c5c0d3e5.png"
+  },
+  {
+    "revision": "ea63a5a4db55ab646d797e2d3775eb89",
+    "url": "/space_digital/static/media/webdev.ea63a5a4.png"
+  }
+]);
